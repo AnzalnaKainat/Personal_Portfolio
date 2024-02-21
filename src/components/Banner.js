d867113d383c8870a02d3aa9e3d014465bfd650a
AnzalnaKainat <anzalnakainat@gmail.com>
2024-02-21 15:20:36 +0500

    second commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,8 +14,8 @@ export const Banner = () => {
       <Container>
         <Col className="aligh-items-center" >
           <span className="tagline">Welcome to my portfolio</span>
-          <h1>Hi! I'm Anzalna Kainat | UX Engineer</h1>
-          <p>In a world driven by user-centric design, I'm Anzalna Kainat, a passionate UX Engineer. I believe in crafting meaningful interactions through a meticulous process that begins with understanding user needs and ends with innovative solutions. With every touch-point, I strive to create experiences that resonate and elevate user engagement in an ever-evolving digital landscape. Complementing my UX expertise, I specialize in front-end development, leveraging technologies to translate design concepts into seamless, intuitive interfaces that captivate users from first click to conversion.</p>
+          <h1>Hi! I'm Anzalna Kainat | UI/UX Designer</h1>
+          <p>In a world driven by user-centric design, I'm Anzalna Kainat, a passionate UI/UX Designer. I believe in crafting meaningful interactions through a meticulous process that begins with understanding user needs and ends with innovative solutions. With every touch-point, I strive to create experiences that resonate and elevate user engagement in an ever-evolving digital landscape. Complementing my UX expertise, I specialize in front-end development, leveraging technologies to translate design concepts into seamless, intuitive interfaces that captivate users from first click to conversion.</p>
           <button onClick={openResume}>My Resume <ArrowRightCircle size={25} /></button>
         </Col>
       </Container>
